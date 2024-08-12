e57b7955547b091f655ad51635495d8065175025
SangHun Kwon <jun00739@naver.com>
2024-08-12 21:39:48 +0900

    main animation
    main page animation

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import styles from "../styles/Main.module.css";
 
 function Main() {
   return (
-    <div>
-      
+    <div className={styles.body}>
+      <div className={`${styles.wave} ${styles.one}`}></div>
+      <div className={`${styles.wave} ${styles.two}`}></div>
+      <div className={`${styles.wave} ${styles.three}`}></div>
     </div>
   )
 }
