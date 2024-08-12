92636fcd3e7e39879883ec924d276fd73c21dcb3
SangHun Kwon <jun00739@naver.com>
2024-08-12 16:40:06 +0900

    초기 설정
    초기 설정

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <CookiesProvider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </CookiesProvider>
+);
