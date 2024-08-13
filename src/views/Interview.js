5a81edc79d78ea3355716f434f88402b7ad250e0
1Fe <dnjscjf0829@naver.com>
2024-08-13 10:01:20 +0900

    [save] Interview 임시저장

--- src/views/Interview.js
+++ src/views/Interview.js
@@ -1,25 +1,57 @@
 import React, { useState } from "react";
 import styles from "../styles/Interview.module.css";
+import mascot5 from "../img/mascot5.png";
 
 const Interview = () => {
     const [isStarted, setIsstarted] = useState(false);
+    const [isInterviewStarted, setIsInterviewStarted] = useState(false);
+    const [category, setCategory] = useState(null);
 
     function handleStartBtn() {
         setIsstarted(true);
     }
 
     return (
-        <div className={styles.Back}>
-            <div className={styles.Container}>
-                <div className={styles.title}>자기소개서 첨삭</div>
-                <div className={styles.content}>
-                    자기소개서 쓰기 많이 어렵지 않나요?
-                    <br /> 용복이가 도와줄거에요 걱정마세요
+        <div>
+            {!isStarted ? (
+                <div className={styles.background}>
+                    <div className={styles.mainWrapper}>
+                        <img
+                            className={styles.mascotPos}
+                            src={mascot5}
+                            alt=""
+                        />
+                        <div className={styles.mainBox}>
+                            <div className={styles.searchTitle1}>
+                                숨겨진 복지 찾기
+                            </div>
+                            <div className={styles.searchTitle2}>
+                                다양한 복지 혜택과 정책이 있지만,
+                                <br />
+                                몰라서 놓친 적이 있으셨나요?
+                                <br />
+                                찾기가 어렵다면 저희 용복이가 찾아줄께요!
+                            </div>
+                            <button
+                                className={styles.startBtn}
+                                onClick={handleStartBtn}
+                            >
+                                시작해보기
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div className={styles.btn} onClick={handleStartBtn}>
-                    시작하기
+            ) : (
+                <div>
+                    {!isInterviewStarted ? (
+                        <div className={styles.background}>
+                            <div className={styles.questionBox}></div>
+                        </div>
+                    ) : (
+                        <div></div>
+                    )}
                 </div>
-            </div>
+            )}
         </div>
     );
 };
