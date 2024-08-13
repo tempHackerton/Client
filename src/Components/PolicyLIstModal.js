3137ff4ce57baa68f2e184cb3df2f695a135f7f0
1Fe <dnjscjf0829@naver.com>
2024-08-13 19:47:34 +0900

    [feat] PolicyListModal 구현

---
+++ src/Components/PolicyLIstModal.js
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import styles from "../styles/Modal/PolicyListModal.module.css";
+import mascot from "../img/mascot1.png";
+import mascotIcon from "../img/mascotIcon.png";
+
+const PolicyListModal = ({ isOpen, onClose, policies }) => {
+    if (!isOpen) return null; // Do not render the modal if it is not open
+
+    const policyList = {
+        policies: [
+            {
+                name: "돌봄 아동 - 대학생 매칭 학습 지원",
+                type: "복지지원",
+                startTime: "상반기 2024.2 / 하반기 2024.8",
+                endTime: "상반기 2024.7 / 하반기 2024.12",
+                target: "18세 ~ 39세 관내 대학(원)생 및 3년 이내 대학 졸업생",
+                description:
+                    "대학생 - 돌봄 아동 매칭을 통한 학습 지도 및 재능 발굴",
+            },
+            {
+                name: "용인시 일자리센터를 통한 청년 취업 지원",
+                type: "일자리창출",
+                startTime: "2024.1",
+                endTime: "2024.12",
+                target: "청년 중 미취업자",
+                description:
+                    "구인구직 상담, 취업 알선, 취업 교육 운영 등 종합적 취업 지원 서비스 제공",
+            },
+            {
+                name: "용인시 청년 창업지원센터 운영",
+                type: "창업지원",
+                startTime: "2024.1",
+                endTime: "2024.12",
+                target: "창업자 및 예비 창업자",
+                description: "창업 성공을 위한 각종 지원 서비스 제공",
+            },
+            {
+                name: "청년 창업 지원금 제공",
+                type: "창업지원",
+                startTime: "2024.1",
+                endTime: "2024.12",
+                target: "창업자 또는 예비 창업자",
+                description: "청년 창업자의 창업 초기 자금 지원",
+            },
+            {
+                name: "청년 창업 지원금 제공",
+                type: "창업지원",
+                startTime: "2024.1",
+                endTime: "2024.12",
+                target: "창업자 또는 예비 창업자",
+                description: "청년 창업자의 창업 초기 자금 지원",
+            },
+            {
+                name: "청년 창업 지원금 제공",
+                type: "창업지원",
+                startTime: "2024.1",
+                endTime: "2024.12",
+                target: "창업자 또는 예비 창업자",
+                description: "청년 창업자의 창업 초기 자금 지원",
+            },
+            {
+                name: "청년 창업 지원금 제공",
+                type: "창업지원",
+                startTime: "2024.1",
+                endTime: "2024.12",
+                target: "창업자 또는 예비 창업자",
+                description: "청년 창업자의 창업 초기 자금 지원",
+            },
+            {
+                name: "청년 창업 지원금 제공",
+                type: "창업지원",
+                startTime: "2024.1",
+                endTime: "2024.12",
+                target: "창업자 또는 예비 창업자",
+                description: "청년 창업자의 창업 초기 자금 지원",
+            },
+        ],
+    };
+
+    return (
+        <div className={styles.modal_overlay}>
+            <div className={styles.container}>
+                <button className={styles.closeButton} onClick={onClose}>
+                    &times;
+                </button>
+                <div className={styles.chatWindow}>
+                    {policyList.policies.map((policy, index) => (
+                        <div key={index} className={styles.message}>
+                            <img
+                                className={styles.avator}
+                                style={{
+                                    width: "2.617vw",
+                                    height: "6.122vh",
+                                    borderRadius: "50%",
+                                    marginLeft: "0.5vw",
+                                }}
+                                src={mascotIcon}
+                                alt=""
+                            />
+                            <div
+                                className={styles.bubble}
+                                onClick={() => {
+                                    console.log(index);
+                                }}
+                            >
+                                <h3>{policy.name}</h3>
+                                {/* <p>
+                                    <strong>Type:</strong> {policy.type}
+                                </p>
+                                <p>
+                                    <strong>Start Time:</strong>{" "}
+                                    {policy.startTime}
+                                </p>
+                                <p>
+                                    <strong>End Time:</strong> {policy.endTime}
+                                </p>
+                                <p>
+                                    <strong>Target Audience:</strong>{" "}
+                                    {policy.target}
+                                </p>
+                                <p>
+                                    <strong>Description:</strong>{" "}
+                                    {policy.description}
+                                </p> */}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PolicyListModal;
--- src/views/Headers.js
+++ src/views/Headers.js
@@ -4,199 +4,210 @@ import styled, { css } from "styled-components";
 import styles from "../styles/Headers.module.css";
 import UseDetectClose from "../hooks/useDetectClose";
 import { useCookies } from "react-cookie";
+import PolicyListModal from "../Components/PolicyLIstModal";
 
 import mascot from "../mascot.png";
 function Headers() {
-	const [LogoutIsOpen, LogoutRef, LogoutHandler] = UseDetectClose(false);
+    const [LogoutIsOpen, LogoutRef, LogoutHandler] = UseDetectClose(false);
     const [aiIsOpen, aiRef, aiHandler] = UseDetectClose(false);
-	const [serviceIsOpen, serviceRef, serviceHandler] = UseDetectClose(false);
-	// const [cookies, setCookie, removeCookie] = useCookies([
-	// 	"token",
-	// 	"socialToken",
-	// ]);
-	const [isuser, setIsuser] = useState(true);
+    const [serviceIsOpen, serviceRef, serviceHandler] = UseDetectClose(false);
+    // const [alarmIsOpen, alarmRef, alarmHandler] = UseDetectClose(false);
+
+    const [open, setOpen] = useState(false);
+
+    // const [cookies, setCookie, removeCookie] = useCookies([
+    // 	"token",
+    // 	"socialToken",
+    // ]);
+    const [isuser, setIsuser] = useState(true);
     //dd
-	function handleHomeClick() {
-		window.location.href = "/";
-	}
+    function handleHomeClick() {
+        window.location.href = "/";
+    }
 
-	// 페이지가 처음 로드될 때만 토큰을 설정하도록 수정
-	// useEffect(() => {
-	// 	const storedSocialToken = cookies.socialToken;
-	// 	if (storedSocialToken && !cookies.token) {
-	// 		setCookie("token", storedSocialToken, {
-	// 			path: "/",
-	// 			domain: "www.vivi-o.site",
-	// 		});
-	// 		console.log("설정 후 토큰 쿠키:", cookies.token);
-	// 	}
-	// }, []);
+    // 페이지가 처음 로드될 때만 토큰을 설정하도록 수정
+    // useEffect(() => {
+    // 	const storedSocialToken = cookies.socialToken;
+    // 	if (storedSocialToken && !cookies.token) {
+    // 		setCookie("token", storedSocialToken, {
+    // 			path: "/",
+    // 			domain: "www.vivi-o.site",
+    // 		});
+    // 		console.log("설정 후 토큰 쿠키:", cookies.token);
+    // 	}
+    // }, []);
 
-	// 토큰 상태를 체크하여 사용자 상태 업데이트
-	// useEffect(() => {
-	// 	if (
-	// 		!cookies.token ||
-	// 		cookies.token === undefined ||
-	// 		cookies.token === "undefined"
-	// 	) {
-	// 		setIsuser(false);
-	// 	} else {
-	// 		setIsuser(true);
-	// 	}
-	// }, [cookies.token]);
+    // 토큰 상태를 체크하여 사용자 상태 업데이트
+    // useEffect(() => {
+    // 	if (
+    // 		!cookies.token ||
+    // 		cookies.token === undefined ||
+    // 		cookies.token === "undefined"
+    // 	) {
+    // 		setIsuser(false);
+    // 	} else {
+    // 		setIsuser(true);
+    // 	}
+    // }, [cookies.token]);
 
-	// 로그아웃 함수
-	function handleLogoutClick() {
-		// window.location.href = "/";
-		// if (cookies.socialToken) {
-		// 	removeCookie("socialToken", { path: "/", domain: "vivi-o.site" });
-		// 	removeCookie("token", { path: "/", domain: "www.vivi-o.site" });
-		// } else {
-		// 	removeCookie("token", { path: "/", domain: "vivi-o.site" });
-		// }
-		// // 쿠키에서 토큰을 제거하고 사용자 상태를 업데이트합니다.
-	}
+    // 로그아웃 함수
+    function handleLogoutClick() {
+        // window.location.href = "/";
+        // if (cookies.socialToken) {
+        // 	removeCookie("socialToken", { path: "/", domain: "vivi-o.site" });
+        // 	removeCookie("token", { path: "/", domain: "www.vivi-o.site" });
+        // } else {
+        // 	removeCookie("token", { path: "/", domain: "vivi-o.site" });
+        // }
+        // // 쿠키에서 토큰을 제거하고 사용자 상태를 업데이트합니다.
+    }
 
-	function handleLoginClick() {
-		window.location.href = "/Login";
-	}
-	function handleSearchClick() {
-		window.location.href = "/Search";
-	}
+    function handleLoginClick() {
+        window.location.href = "/Login";
+    }
+    function handleSearchClick() {
+        window.location.href = "/Search";
+    }
 
-  return (
-    <div
-		style={{
-			borderTop: 0,
-			borderLeft: 0,
-			borderRight: 0,
-			borderBottom: 2,
-			borderStyle: "solid",
-			borderColor: "black",
-		}}
-		className={styles.headercontainer}
-	>
-		<div className={styles.logoleft}>
-			<img
-				onClick={handleHomeClick}
-				style={{ width :'auto', height: '11vh' }}
-				src={mascot}
-				alt="Yong bok"
-			/>
-		</div>
-        <div className={styles.navelement} style={{ color: "#f1f1f1" }}>
-			<div onClick={handleHomeClick}>
-				<p>Home</p>
-			</div>
-			<div onClick={serviceHandler} ref={serviceRef}>
-				<p>Service</p>
-				<Menu isDropped={serviceIsOpen}>
-					<Ul>
-						<Li>
-							<LinkWrapper onClick={handleSearchClick}>
-								숨겨진 복지 찾기
-							</LinkWrapper>
-						</Li>
-					</Ul>
-				</Menu>
-			</div>
-			<div>
-				<p>alarm</p>
-			</div>
-			{isuser &&(
-				<div onClick={LogoutHandler} ref={LogoutRef}>
-				<p>장준용님 환영합니다.</p>
-				<Menu isDropped={LogoutIsOpen}>
-					<Ul>
-						<Li>
-							<LinkWrapper onClick={handleLogoutClick}>
-								로그아웃
-							</LinkWrapper>
-						</Li>
-					</Ul>
-				</Menu>
-			</div>
-			)}
-			{!isuser &&(
-				<div><p onClick={handleLoginClick}>Login/Signup</p></div>
-			)}
-		</div>
-    </div>
-  )
+    return (
+        <div
+            style={{
+                borderTop: 0,
+                borderLeft: 0,
+                borderRight: 0,
+                borderBottom: 2,
+                borderStyle: "solid",
+                borderColor: "black",
+            }}
+            className={styles.headercontainer}
+        >
+            <PolicyListModal
+                isOpen={open}
+                onClose={() => setOpen(false)}
+            ></PolicyListModal>
+            <div className={styles.logoleft}>
+                <img
+                    onClick={handleHomeClick}
+                    style={{ width: "auto", height: "11vh" }}
+                    src={mascot}
+                    alt="Yong bok"
+                />
+            </div>
+            <div className={styles.navelement} style={{ color: "#f1f1f1" }}>
+                <div onClick={handleHomeClick}>
+                    <p>Home</p>
+                </div>
+                <div onClick={serviceHandler}>
+                    <p>Service</p>
+                    <Menu isDropped={serviceIsOpen}>
+                        <Ul>
+                            <Li>
+                                <LinkWrapper onClick={handleSearchClick}>
+                                    숨겨진 복지 찾기
+                                </LinkWrapper>
+                            </Li>
+                        </Ul>
+                    </Menu>
+                </div>
+                <div onClick={() => setOpen(true)}>
+                    <p>alarm</p>
+                </div>
+                {isuser && (
+                    <div onClick={LogoutHandler} ref={LogoutRef}>
+                        <p>장준용님 환영합니다.</p>
+                        <Menu isDropped={LogoutIsOpen}>
+                            <Ul>
+                                <Li>
+                                    <LinkWrapper onClick={handleLogoutClick}>
+                                        로그아웃
+                                    </LinkWrapper>
+                                </Li>
+                            </Ul>
+                        </Menu>
+                    </div>
+                )}
+                {!isuser && (
+                    <div>
+                        <p onClick={handleLoginClick}>Login/Signup</p>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
-export default Headers
+export default Headers;
 
 const Menu = styled.div`
-	background: #A8D9BE;
-	position: absolute;
-	top: 100%;
-	width: 250px;
-	text-align: center;
-	box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
-	border-radius: 20px;
-	opacity: 0;
-	visibility: hidden;
-	transform: translateY(-20px);
-	transition: opacity 0.4s ease, transform 0.4s ease, visibility 0.4s;
-	z-index: 9;
+    background: #a8d9be;
+    position: absolute;
+    top: 100%;
+    width: 250px;
+    text-align: center;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+    border-radius: 20px;
+    opacity: 0;
+    visibility: hidden;
+    transform: translateY(-20px);
+    transition: opacity 0.4s ease, transform 0.4s ease, visibility 0.4s;
+    z-index: 9;
 
-	font-family: "NanumSquareRoundOTF";
-	font-style: normal;
-	font-weight: 800;
-	font-size: 22px;
-	line-height: 140%;
-	/* identical to box height, or 28px */
-	letter-spacing: 0.02em;
-	text-transform: capitalize;
+    font-family: "NanumSquareRoundOTF";
+    font-style: normal;
+    font-weight: 800;
+    font-size: 22px;
+    line-height: 140%;
+    /* identical to box height, or 28px */
+    letter-spacing: 0.02em;
+    text-transform: capitalize;
 
-	color: #ffffff;
+    color: #ffffff;
 
-	text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
-	&:after {
-		content: "";
-		height: 0;
-		width: 0;
-		position: absolute;
-		top: -3px;
-		transform: translateY(-50%);
-		border: 20px solid transparent;
-		border-top-width: 0;
-		border-bottom-color: #A8D9BE;
-	}
+    &:after {
+        content: "";
+        height: 0;
+        width: 0;
+        position: absolute;
+        top: -3px;
+        transform: translateY(-50%);
+        border: 20px solid transparent;
+        border-top-width: 0;
+        border-bottom-color: #a8d9be;
+    }
 
-	${({ isDropped }) =>
-		isDropped &&
-		css`
-			opacity: 1;
-			visibility: visible;
-			transform: translateY(0);
-		`};
+    ${({ isDropped }) =>
+        isDropped &&
+        css`
+            opacity: 1;
+            visibility: visible;
+            transform: translateY(0);
+        `};
 `;
 
 const Ul = styled.ul`
-	& > li {
-		margin-bottom: 10px;
-	}
+    & > li {
+        margin-bottom: 10px;
+    }
 
-	& > li:first-of-type {
-		margin-top: 10px;
-	}
+    & > li:first-of-type {
+        margin-top: 10px;
+    }
 
-	list-style-type: none;
-	padding: 0;
-	margin: 0;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	align-items: center;
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 `;
 
 const Li = styled.li``;
 
 const LinkWrapper = styled.a`
-	font-size: 22px;
-	text-decoration: none;
-	color: white;
+    font-size: 22px;
+    text-decoration: none;
+    color: white;
 `;
